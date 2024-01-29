a2f74f948fd40f50757fa1afff66bc91510dc242
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-01-29 13:42:12 -0300

    finalizando testes

--- src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
+++ src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
@@ -2,6 +2,8 @@ package com.generation.blogpessoal.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -67,4 +69,41 @@ public class UsuarioControllerTest {
 
 		assertEquals(HttpStatus.BAD_REQUEST, corpoResposta.getStatusCode());
 	}
+
+	@Test
+	@DisplayName("Atualizar um Usuário")
+	public void deveAtualizarUmUsuario() {
+
+		Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(
+				new Usuario(0L, "Juliana Andrews", "juliana_andrews@email.com.br", "juliana123", "-"));
+
+		Usuario usuarioUpdate = new Usuario(usuarioCadastrado.get().getId(), "Juliana Andrews Ramos",
+				"juliana_ramos@email.com.br", "juliana123", "-");
+
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuarioUpdate);
+
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate.withBasicAuth("root@root.com", "rootroot")
+				.exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
+
+		assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
+
+	}
+
+	@Test
+	@DisplayName("Listar todos os Usuários")
+	public void deveMostrarTodosUsuarios() {
+
+		usuarioService.cadastrarUsuario(
+				new Usuario(0L, "Sabrina Sanches", "sabrina_sanches@email.com.br", "sabrina123", "-"));
+
+		usuarioService.cadastrarUsuario(
+				new Usuario(0L, "Ricardo Marques", "ricardo_marques@email.com.br", "ricardo123", "-"));
+
+		ResponseEntity<String> resposta = testRestTemplate.withBasicAuth("root@root.com", "rootroot")
+				.exchange("/usuarios/all", HttpMethod.GET, null, String.class);
+
+		assertEquals(HttpStatus.OK, resposta.getStatusCode());
+
+	}
+
 }
